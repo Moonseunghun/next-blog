146a8a05cfc38505da79a7fdaf74beb09e4d576b
moonseunghun <mangokkaksae2@gmail.com>
2023-06-24 17:57:25 +0900

    Chore : page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <p>바지 제품 소개 페이지</p>;
+};
+
+export default page;
---
+++ src/app/products/pants/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <h1>이것은 슬렉스</h1>;
+};
+
+export default index;
