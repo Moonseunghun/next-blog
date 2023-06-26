57db26552d617581348ba5d194f1e522420a4183
moonseunghun <mangokkaksae2@gmail.com>
2023-06-26 17:39:27 +0900

    Chore : reset

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const index = () => {
-  return <h1>이것은 슬렉스</h1>;
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const index = ({ params }: Props) => {
+  return <h1>{params.slug}이것은 슬렉스</h1>;
 };
 
 export default index;
